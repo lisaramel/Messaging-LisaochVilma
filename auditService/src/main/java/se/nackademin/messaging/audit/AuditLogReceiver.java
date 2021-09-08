8a8825d47f04bb37a1a648d79b08990c3219316c
Dan Eidmark <dan.eidmark@gmail.com>
2021-09-08 21:53:33 +0200

    Add Listener as part of task 2

--- auditService/src/main/java/se/nackademin/messaging/audit/AuditLogReceiver.java
+++ auditService/src/main/java/se/nackademin/messaging/audit/AuditLogReceiver.java
@@ -13,15 +13,19 @@ public class AuditLogReceiver {
     @Autowired
     AuditLogRepository auditLogRepository;
 
-    @RabbitListener(queues = "audit-log")
     public void receiveMessage(AuditEvent event) {
         LOG.info("Received message! {}", event);
         /* TODO: Uppgift 2: Spara eventet!
-            För att lyssna på events räcker det med @RabbitListner!
+            För att lyssna på events räcker det med @RabbitListner! Annotera denna metod med:
+             @RabbitListener(queues = "audit-log") så kommer den automagiskt att ropas på
+             när ett message hamnar på den queuen!
+
+            Om ni nu kör testet borde ni få en utskrift i loggen "Received message" även to testet failar.
 
             När vi får in ett event så vill vi spara det i auditLogRepository.
 
             Översätt DTOn AuditEvent till vårat domänobjekt AuditEntry och spara det i databasen.
+            Finns ett auditLogRepository här ni kan använda er av för att spara AuditEntries.
 
             För att se att allt fungerar kör testet AuditApplicationTest
          */
